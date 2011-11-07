f1d9b74c12f873ab7aa126c804eb53e0b3ccb98f
unknown <jeffhand@.redmond.corp.microsoft.com>
2011-11-06 20:55:41 -0800

    Added RepeatedString

---
+++ RepeatedString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ThirtyFour
+{
+    public static class RepeatedString
+    {
+        public static string LongestRepeatedString(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentNullException("input");
+            }
+
+            var repeats = new List<Tuple<string, int>>();
+
+            for (var x = 0; x < input.Length; ++x)
+            {
+                for (var l = 1; x + l <= input.Length; ++l)
+                {
+                    var pattern = input.Substring(x, l);
+                    repeats.Add(Tuple.Create(pattern, Regex.Matches(input, pattern).Count));
+                }
+            }
+
+            var longest = repeats
+                .Where(r => r.Item2 > 1)
+                .OrderByDescending(r => r.Item1.Length)
+                .ThenByDescending(r => r.Item2)
+                .FirstOrDefault();
+
+            return longest != null ? longest.Item1 : null;
+        }
+    }
+}
---
+++ RepeatedStringTests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThirtyFour
+{
+    [TestClass]
+    public class RepeatedStringTests
+    {
+        [TestMethod]
+        public void Null()
+        {
+            try
+            {
+                string input = null;
+                input.LongestRepeatedString();
+                Assert.Fail("Expected ArgumentNullException. None caught.");
+            }
+            catch (ArgumentNullException)
+            {
+                // Success
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(string.Format("ArgumentNullException expected.  Caught: {0}", ex.GetType()));
+            }
+        }
+
+        [TestMethod]
+        public void EmptyString()
+        {
+            try
+            {
+                string input = string.Empty;
+                input.LongestRepeatedString();
+                Assert.Fail("Expected ArgumentNullException. None caught.");
+            }
+            catch (ArgumentNullException)
+            {
+                // Success
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(string.Format("ArgumentNullException expected.  Caught: {0}", ex.GetType()));
+            }
+        }
+
+        [TestMethod]
+        public void x_yields_null()
+        {
+            string input = "x";
+            string longest = input.LongestRepeatedString();
+
+            Assert.IsNull(longest);
+        }
+
+        [TestMethod]
+        public void xx_yields_x()
+        {
+            string input = "xx";
+            string longest = input.LongestRepeatedString();
+
+            Assert.AreEqual("x", longest);
+        }
+
+        [TestMethod]
+        public void xxx_yields_x()
+        {
+            string input = "xxx";
+            string longest = input.LongestRepeatedString();
+
+            Assert.AreEqual("x", longest);
+        }
+
+        [TestMethod]
+        public void xxy_yields_x()
+        {
+            string input = "xxy";
+            string longest = input.LongestRepeatedString();
+
+            Assert.AreEqual("x", longest);
+        }
+
+        [TestMethod]
+        public void xxyy_yields_x()
+        {
+            string input = "xxyy";
+            string longest = input.LongestRepeatedString();
+
+            Assert.AreEqual("x", longest);
+        }
+
+        [TestMethod]
+        public void xxyxx_yields_xx()
+        {
+            string input = "xxyxx";
+            string longest = input.LongestRepeatedString();
+
+            Assert.AreEqual("xx", longest);
+        }
+
+        [TestMethod]
+        public void xyxyxy_yields_xy()
+        {
+            string input = "xyxyxy";
+            string longest = input.LongestRepeatedString();
+
+            Assert.AreEqual("xy", longest);
+        }
+
+        [TestMethod]
+        public void xyxyxyxyxyxyxyx_yields_xyxyxyx()
+        {
+            string input = "xyxyxyxyxyxyxyx";
+            string longest = input.LongestRepeatedString();
+
+            Assert.AreEqual("xyxyxyx", longest);
+        }
+    }
+}

6c7f7545b3992b2420474d1c6d9239eaf2c19a19
Jeff Handley <jeff@jeffhandley.com>
2012-08-13 19:18:36 -0700

    Added Anagram

---
+++ Anagram.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThirtyFour
+{
+    public static class Anagram
+    {
+        internal static bool AreWordsAnagrams(string word1, string word2)
+        {
+            if (string.IsNullOrWhiteSpace(word1) || string.IsNullOrWhiteSpace(word2))
+            {
+                return false;
+            }
+
+            word1 = Sanitize(word1);
+            word2 = Sanitize(word2);
+
+            if (string.IsNullOrEmpty(word1) || string.IsNullOrEmpty(word2))
+            {
+                return false;
+            }
+
+            if (word1.Length != word2.Length)
+            {
+                return false;
+            }
+
+            var word1chars = new Dictionary<char, int>();
+            var word2chars = new Dictionary<char, int>();
+
+            for (int x = 0; x < word1.Length; ++x)
+            {
+                CountCharacter(word1, word1chars, x);
+                CountCharacter(word2, word2chars, x);
+            }
+
+            return DictionariesEqual(word1chars, word2chars);
+        }
+
+        private static bool DictionariesEqual(Dictionary<char, int> word1chars, Dictionary<char, int> word2chars)
+        {
+            if (word1chars.Keys.Count != word2chars.Keys.Count)
+            {
+                return false;
+            }
+
+            foreach (char key in word1chars.Keys)
+            {
+                if (!word2chars.ContainsKey(key) || word2chars[key] != word1chars[key])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void CountCharacter(string word, Dictionary<char, int> wordchars, int x)
+        {
+            var wordchar = word[x];
+
+            if (wordchars.ContainsKey(wordchar))
+            {
+                ++wordchars[wordchar];
+            }
+            else
+            {
+                wordchars.Add(wordchar, 1);
+            }
+        }
+
+        private static string Sanitize(string word)
+        {
+            return word.ToLowerInvariant().Replace(" ", "");
+        }
+    }
+}
---
+++ AnagramTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThirtyFour
+{
+    /// <summary>
+    /// Summary description for AnagramTests
+    /// </summary>
+    [TestClass]
+    public class AnagramTests
+    {
+        [TestMethod]
+        public void ReversedWordIsAnagram()
+        {
+            Assert.IsTrue(Anagram.AreWordsAnagrams("abcde", "edcba"));
+        }
+
+        [TestMethod]
+        public void ShiftedWordIsAnagram()
+        {
+            Assert.IsTrue(Anagram.AreWordsAnagrams("abcde", "cdeba"));
+        }
+
+        [TestMethod]
+        public void MixedWordIsAnagram()
+        {
+            Assert.IsTrue(Anagram.AreWordsAnagrams("abcde", "acebd"));
+        }
+
+        [TestMethod]
+        public void ExtraLettersRuleItOut()
+        {
+            Assert.IsFalse(Anagram.AreWordsAnagrams("abcde", "abcdef"));
+        }
+
+        [TestMethod]
+        public void MissingLettersRuleItOut()
+        {
+            Assert.IsFalse(Anagram.AreWordsAnagrams("abcde", "abcd"));
+        }
+
+        [TestMethod]
+        public void ExtraLetterOccurrenceRulesItOut()
+        {
+            Assert.IsFalse(Anagram.AreWordsAnagrams("abcde", "abcdee"));
+        }
+
+        [TestMethod]
+        public void MismatchIsNotAnAnagram()
+        {
+            Assert.IsFalse(Anagram.AreWordsAnagrams("abcde", "fghij"));
+        }
+
+        [TestMethod]
+        public void CaseDoesNotMatter()
+        {
+            Assert.IsTrue(Anagram.AreWordsAnagrams("ABCDE", "abcde"));
+        }
+
+        [TestMethod]
+        public void TwoWordsCanMatchTwoWords()
+        {
+            Assert.IsTrue(Anagram.AreWordsAnagrams("abcde fghij", "cdefghi jab"));
+        }
+
+        [TestMethod]
+        public void TwoWordsCanMatchOneWord()
+        {
+            Assert.IsTrue(Anagram.AreWordsAnagrams("abcde fghij", "cdefghijab")); 
+        }
+
+        [TestMethod]
+        public void NullDoesNotThrowForFirstWord()
+        {
+            Assert.IsFalse(Anagram.AreWordsAnagrams(null, "abcde"));
+        }
+
+        [TestMethod]
+        public void NullDoesNotThrowForSecondWord()
+        {
+            Assert.IsFalse(Anagram.AreWordsAnagrams("abcde", null));
+        }
+
+        [TestMethod]
+        public void EmptyWordsDoNotMatch()
+        {
+            Assert.IsFalse(Anagram.AreWordsAnagrams(string.Empty, string.Empty));
+        }
+
+        [TestMethod]
+        public void NullsDoNotMatch()
+        {
+            Assert.IsFalse(Anagram.AreWordsAnagrams(null, null));
+        }
+
+        [TestMethod]
+        public void SpacesDoNotMatch()
+        {
+            Assert.IsFalse(Anagram.AreWordsAnagrams(" ", " "));
+        }
+    }
+}

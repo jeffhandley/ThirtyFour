a49abfbef63e451f24fc51fc5028e1a7c499c2b0
Jeff Handley <jeff@jeffhandley.com>
2012-09-27 07:57:31 -0700

    Improvements on Anagram

--- Anagram.cs
+++ Anagram.cs
@@ -17,63 +17,52 @@ namespace ThirtyFour
             word1 = Sanitize(word1);
             word2 = Sanitize(word2);
 
-            if (string.IsNullOrEmpty(word1) || string.IsNullOrEmpty(word2))
-            {
-                return false;
-            }
-
             if (word1.Length != word2.Length)
             {
                 return false;
             }
 
-            var word1chars = new Dictionary<char, int>();
-            var word2chars = new Dictionary<char, int>();
+            var charCounts = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
 
             for (int x = 0; x < word1.Length; ++x)
             {
-                CountCharacter(word1, word1chars, x);
-                CountCharacter(word2, word2chars, x);
-            }
+                string char1 = word1[x].ToString();
+                string char2 = word2[x].ToString();
 
-            return DictionariesEqual(word1chars, word2chars);
-        }
-
-        private static bool DictionariesEqual(Dictionary<char, int> word1chars, Dictionary<char, int> word2chars)
-        {
-            if (word1chars.Keys.Count != word2chars.Keys.Count)
-            {
-                return false;
-            }
-
-            foreach (char key in word1chars.Keys)
-            {
-                if (!word2chars.ContainsKey(key) || word2chars[key] != word1chars[key])
+                if (char1 != char2)
                 {
-                    return false;
+                    if (charCounts.ContainsKey(char1))
+                    {
+                        if (++charCounts[char1] == 0)
+                        {
+                            charCounts.Remove(char1);
+                        }
+                    }
+                    else
+                    {
+                        charCounts.Add(char1, 1);
+                    }
+
+                    if (charCounts.ContainsKey(char2))
+                    {
+                        if (--charCounts[char2] == 0)
+                        {
+                            charCounts.Remove(char2);
+                        }
+                    }
+                    else
+                    {
+                        charCounts.Add(char2, -1);
+                    }
                 }
             }
 
-            return true;
-        }
-
-        private static void CountCharacter(string word, Dictionary<char, int> wordchars, int x)
-        {
-            var wordchar = word[x];
-
-            if (wordchars.ContainsKey(wordchar))
-            {
-                ++wordchars[wordchar];
-            }
-            else
-            {
-                wordchars.Add(wordchar, 1);
-            }
+            return charCounts.Count == 0;
         }
 
         private static string Sanitize(string word)
         {
-            return word.ToLowerInvariant().Replace(" ", "");
+            return word.Replace(" ", "");
         }
     }
 }

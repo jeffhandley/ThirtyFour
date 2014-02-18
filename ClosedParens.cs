b4d00955bac3d555f0fc61a303f189c994a1d634
= <=>
2014-02-18 01:35:09 -0800

    Paren Matching

---
+++ ClosedParens.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThirtyFour
+{
+    public static class ClosedParens
+    {
+        public static bool HasMismatchedParens(this string statement)
+        {
+            return MatchParens(statement).HasMismatch;
+        }
+
+        public static int GetParenPairCount(this string statement)
+        {
+            return MatchParens(statement).ClosedPairCount;
+        }
+
+        public static int GetUnopenedParenCount(this string statement)
+        {
+            return MatchParens(statement).UnopenedCloseParens;
+        }
+
+        private struct ParenMatching
+        {
+            public int ClosedPairCount;
+            public int UnclosedOpenParens;
+            public int UnopenedCloseParens;
+            public bool HasMismatch
+            {
+                get
+                {
+                    return (UnclosedOpenParens > 0 || UnopenedCloseParens > 0);
+                }
+            }
+        }
+
+        private static ParenMatching MatchParens(string statement)
+        {
+            Stack opened = new Stack();
+            ParenMatching matching = new ParenMatching();
+
+            foreach (var c in statement)
+            {
+                if (c == '(')
+                {
+                    opened.Push(null);
+                }
+                else if (c == ')')
+                {
+                    if (opened.Count == 0)
+                    {
+                        ++matching.UnopenedCloseParens;
+                    }
+                    else
+                    {
+                        opened.Pop();
+                        ++matching.ClosedPairCount;
+                    }
+                }
+            }
+
+            matching.UnclosedOpenParens += opened.Count;
+            return matching;
+        }
+    }
+}
---
+++ ClosedParensTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThirtyFour
+{
+    [TestClass]
+    public class ClosedParensTests
+    {
+        [TestMethod]
+        public void NoParensHasNoUnclosedParens()
+        {
+            var noParens = "This string has no Parens.";
+            Assert.IsFalse(noParens.HasMismatchedParens());
+        }
+
+        [TestMethod]
+        public void NoParensHasNoPairs()
+        {
+            var noParens = "This string has no Parens.";
+            Assert.AreEqual(0, noParens.GetParenPairCount());
+        }
+
+        [TestMethod]
+        public void SinglePairHasNoUnclosedParens()
+        {
+            var openClose = "This string has (one pair) of Parens.";
+            Assert.IsFalse(openClose.HasMismatchedParens());
+        }
+
+        [TestMethod]
+        public void OpenCloseHasOnePair()
+        {
+            var openClose = "This string has (one pair) of Parens.";
+            Assert.AreEqual(1, openClose.GetParenPairCount());
+        }
+
+        [TestMethod]
+        public void OpenCloseOpenCloseHasNoUnclosedParens()
+        {
+            var openCloseOpenClose = "This (string) has (two pairs) of Parens.";
+            Assert.IsFalse(openCloseOpenClose.HasMismatchedParens());
+        }
+
+        [TestMethod]
+        public void OpenCloseOpenCloseHasTwoPairs()
+        {
+            var openCloseOpenClose = "This (string) has (two pairs) of Parens.";
+            Assert.AreEqual(2, openCloseOpenClose.GetParenPairCount());
+        }
+
+        [TestMethod]
+        public void OpenHasUnclosedPair()
+        {
+            var open = "This string has (a single Paren.";
+            Assert.IsTrue(open.HasMismatchedParens());
+        }
+
+        [TestMethod]
+        public void OpenHasNoPairs()
+        {
+            var open = "This string has (a single Paren.";
+            Assert.AreEqual(0, open.GetParenPairCount());
+        }
+
+        [TestMethod]
+        public void ThreeParensHasUnclosedPair()
+        {
+            var openCloseOpen = "This (string) has (three Parens.";
+            Assert.IsTrue(openCloseOpen.HasMismatchedParens());
+        }
+
+        [TestMethod]
+        public void OpenCloseOpenHasOnePair()
+        {
+            var openCloseOpen = "This (string) has (three Parens.";
+            Assert.AreEqual(1, openCloseOpen.GetParenPairCount());            
+        }
+
+        [TestMethod]
+        public void CloseCloseOpenHasTwoUnopened()
+        {
+            var closeCloseOpen = "This )) string ( has two unopened Parens.";
+            Assert.AreEqual(2, closeCloseOpen.GetUnopenedParenCount());
+        }
+
+        [TestMethod]
+        public void OpenOpenCloseCloseHasNoUnclosedPairs()
+        {
+            var openOpenCloseClose = "This (string has (two) pairs).";
+            Assert.IsFalse(openOpenCloseClose.HasMismatchedParens());
+        }
+
+        [TestMethod]
+        public void OpenOpenCloseCloseHasTwoPairs()
+        {
+            var openOpenCloseClose = "This (string has (two) pairs).";
+            Assert.AreEqual(2, openOpenCloseClose.GetParenPairCount());
+        }
+    }
+}
